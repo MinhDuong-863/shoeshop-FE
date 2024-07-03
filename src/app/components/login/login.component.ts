a2e32b1767fd63d1a266ef6ca1826002f0b5fd2c
MinhDuong <luceattran@gmail.com>
2024-07-03 11:17:15 +0700

    ssFE01 | role service

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -5,6 +5,8 @@ import { UserService } from '../../services/user.service';
 import { LoginDTO } from '../../dtos/user/login.dto';
 import { LoginResponse} from '../../responses/user/login.response';
 import { TokenService } from '../../services/token.service';
+import { RoleService } from '../../services/role.service';
+import { Role } from '../../models/role';
 
 @Component({
   selector: 'app-login',
@@ -16,18 +18,39 @@ export class LoginComponent {
 
   phone: string;
   password: string;
-  role: number;
   isPasswordVisible: boolean;
+  roleBegin: number;
+
+  roles: Role[] = [];
+  rememberMe: boolean;
+  selectedRole: Role | undefined;
 
   constructor(
     private router: Router, 
     private userService: UserService,
-    private tokenService: TokenService
+    private tokenService: TokenService,
+    private roleService: RoleService
   ){
     this.phone='';
     this.password = '';
-    this.role = 2;
     this.isPasswordVisible = false;
+    this.roleBegin = -1;
+    this.rememberMe = false;
+  }
+
+  ngOnInit(){
+    debugger
+    this.roleService.getRoles().subscribe({
+      next: (roles: Role[]) => {
+        debugger
+        this.roles = roles;
+        this.selectedRole = roles.length > 0 ? roles[0]: undefined;
+      },
+      error: (error: any) => {
+        debugger
+        console.error('Error getting roles: ', error);
+      }
+    });
   }
 
   login(){
@@ -39,7 +62,9 @@ export class LoginComponent {
       next: (response: LoginResponse) => {
         debugger
         const {token} = response
-        this.tokenService.setToken(token);
+        if(this.rememberMe){
+          this.tokenService.setToken(token);  
+        }
         //this.router.navigate(['/login']);
       }, complete: () => {
         debugger
