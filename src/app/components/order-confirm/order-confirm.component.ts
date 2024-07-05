e9a0b9022a5a80f8e7bdbb1f318a5371c20d1e7b
MinhDuong <luceattran@gmail.com>
2024-07-05 22:56:46 +0700

    ssFE03 | Order confirm

--- src/app/components/order-confirm/order-confirm.component.ts
+++ src/app/components/order-confirm/order-confirm.component.ts
@@ -1,10 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../../models/product';
+import { CartService } from '../../services/cart.service';
+import { ProductService } from '../../services/product.service';
+import { environment } from '../../environments/environment';
 
 @Component({
   selector: 'app-order-confirm',
   templateUrl: './order-confirm.component.html',
   styleUrl: './order-confirm.component.scss'
 })
-export class OrderConfirmComponent {
+export class OrderConfirmComponent implements OnInit {
+  cartItems: { product: Product, quantity: number }[] = [];
+  totalAmount: number = 0;
 
+  constructor(
+    private cartService: CartService,
+    private productService: ProductService,
+  ) { }
+
+  ngOnInit(): void {
+    debugger
+    const cart = this.cartService.getCart();
+    const productIds = Array.from(cart.keys());
+    if (productIds.length === 0) {
+      return;
+    }
+    // gọi service để lấy thông tin sản phẩm từ productIds
+    this.productService.getProductByIds(productIds).subscribe({
+      next: (products) => {
+        this.cartItems = productIds.map((productId) => {
+          const product = products.find((p) => p.id === productId);
+          if (product) {
+            product.thumbnail = `${environment.apiBaseUrl}/products/images/${product.thumbnail}`;
+          }
+          return {
+            product: product!,
+            quantity: cart.get(productId)!
+          };
+        });
+      },
+      complete: () => {
+        debugger;
+        this.calculateTotal()
+      },
+      error: (error: any) => {
+        debugger;
+        console.error('Error fetching detail:', error);
+      }
+    });
+  }
+  calculateTotal(): number{
+    return this.totalAmount=this.cartItems.reduce((total, item) => total + item.product.price * item.quantity, 0);
+  }
+  formatCurrency(amount: number): string {
+    return new Intl.NumberFormat('vi-VN', { style: 'currency', currency: 'VND' }).format(amount);
+  }
 }
